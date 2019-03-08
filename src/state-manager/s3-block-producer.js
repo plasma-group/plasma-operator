445f15cd2b70ca64c4e7aee52a3f822acc108fbe
Voxelot <brandonkite92@gmail.com>
2019-03-08 01:24:08 -0800

    filesystem integration & queuing for s3 uploader

--- src/state-manager/s3-block-producer.js
+++ src/state-manager/s3-block-producer.js
@@ -3,19 +3,35 @@
 const fs = require('fs-extra')
 const path = require('path')
 const klaw = require('klaw')
+const chokidar = require('chokidar')
 const AWS = require('aws-sdk')
-const S3 = new AWS.S3({ apiVersion: '2006-03-01' })
+const _ = require('lodash')
+let S3
+
+const TEMP_FILE_NAME = 'tmp-tx-log.bin'
 
 class S3BlockProducer {
-  constructor (txBucketName, txLogDirectory) {
+  /**
+   * S3 Block Producer monitors for newly created block files and uploads them to S3. 
+   * It will also sync existing block files to S3.
+   * 
+   * It's designed to operate on filesystem events, allowing it to run completely decoupled
+   * from the state-service. 
+   * @param {*} txBucketName The name of the S3 bucket for storing blocks
+   * @param {*} txLogDirectory The directory containing blocks
+   * @param {*} fileListeners Listeners to trigger when new block files are added
+   */
+  constructor (txBucketName, txLogDirectory, fileListeners) {
     this.txBucketName = txBucketName
     this.txLogDirectory = txLogDirectory
     this.uploadQueue = []
+    this.fileListeners = fileListeners || []
     // block producer will silently no-op if not configured.
     this.active = false
   }
 
   async init () {
+    S3 = new AWS.S3({ apiVersion: '2006-03-01' })
     // Validate txLogDirectory exists
     await fs.exists(path.join(__dirname, this.txLogDirectory))
 
@@ -23,29 +39,60 @@ class S3BlockProducer {
     if (this.txBucketName.length > 0) {
       try {
         await S3.headBucket({ Bucket: this.txBucketName }).promise()
-          active = true
+        this.active = true
       } catch (e) {
-          throw new Error(`S3 bucket ${this.txBucketName} non-existent or inaccessible.`)
+        console.log(e)
+        throw new Error(`S3 bucket ${this.txBucketName} non-existent or inaccessible.`)
       }
       
-      // Fill transfer queue backlog
-      await initUploadBacklog()
-
       // Setup file monitor for new blocks
+      await this.setupFileMonitor()
+
+      // Fill transfer queue backlog
+      await this.initUploadBacklog()
+
     }
+  }
+  
+  async setupFileMonitor () {
+    let watcher = chokidar.watch(this.txLogDirectory, {
+      ignored: /(^|[\/\\])\../,
+      persistent: true,
+      // don't trigger any events for pre-exisiting files
+      ignoreInitial: true
+    })
+    const watcherSetup = () => new Promise((resolve) => {
+      watcher
+      .on('add', path => {
+        if (path.indexOf(TEMP_FILE_NAME) == -1) {
+          // prevent duplicate entries
+          if (_.findIndex(this.uploadQueue, (item) => item == path) == -1) {
+            this.uploadQueue.push(path)
+          }
+          // fire the file listeners - mainly for integration testing
+          for (const listener of this.fileListeners) {
+            listener(path)
+          }
+        }
+      })
+      .on('ready', () => {
+        resolve()
+      })
+    })
+    await watcherSetup()
   }
 
   // Compare local filesystem with S3 to find files that need to be uploaded
   async initUploadBacklog () {
     // Fetch last uploaded tx log to s3
-    async function getHighestS3Block() {
+    async function getHighestS3Block(bucketName) {
       // iterate listItem api till highest block # is found
-      let topKey = null;
+      let topKey = null
       let params = {
-        Bucket: this.txBucketName,
+        Bucket: bucketName,
         MaxKeys: 1000,
       }
-      let s3ItemListResult = { IsTruncated: true };
+      let s3ItemListResult = { IsTruncated: true }
 
       while (s3ItemListResult.IsTruncated) {
         s3ItemListResult = await S3.listObjectsV2(params).promise()
@@ -53,27 +100,33 @@ class S3BlockProducer {
         // find max block number
         topKey = s3ItemListResult.Contents
                   .map(item => item.Key)  
-                  .reduce((prev, curr) => curr > prev ? curr : prev, topKey)
+                  .reduce((prev, curr) => curr > prev || prev == null ? curr : prev,
+                          topKey)
       }
       
       return topKey
     }
     // get the top key stored in S3
-    const topS3Key = getHighestS3Block()
+    const topS3Key = await getHighestS3Block(this.txBucketName)
+
+    const backlog = []
 
     // iterate through files currently in the directory to queue items for upload
-    const dirStream = klaw(path.join(__dirname, this.txLogDirectory))
+    const dirStream = klaw(this.txLogDirectory)
     for await (const item of dirStream) {
       if (!item.stats.isDirectory()) {
         let key = path.basename(item.path)
-        if (key > topS3Key) {
-          this.uploadQueue.push(item.path)
+        // dont include temp files in queue
+        if ((!topS3Key || key > topS3Key) && key.indexOf(TEMP_FILE_NAME) == -1) {
+          backlog.push(item.path)
         }
       }
     }
 
     // ensure blocks are uploaded in ascending order
+    this.uploadQueue = backlog.concat(this.uploadQueue)
     this.uploadQueue.sort()
   }
-
 }
+
+module.exports = S3BlockProducer
---
+++ test/test-state-manager/s3-producer-test.js
@@ -0,0 +1,179 @@
+/* eslint-env mocha */
+
+// unit tests for S3 uploading / filesystem
+'use strict'
+
+const _ = require('lodash')
+const sysPath = require('path')
+const rimraf = require('rimraf')
+const fs = require('fs-extra')
+const chokidar = require('chokidar')
+const chai = require('chai')
+const sinon = require('sinon')
+chai.should()
+chai.use(require('sinon-chai'))
+const AWS = require('aws-sdk-mock')
+const web3 = require('web3')
+const S3BlockProducer = require('../../src/state-manager/s3-block-producer')
+const BLOCKNUMBER_BYTE_SIZE = require('./../../src/constants').BLOCKNUMBER_BYTE_SIZE
+const BN = web3.utils.BN
+
+process.env.CHOKIDAR_USEPOLLING = true
+
+const expect = chai.expect
+
+function getFixturePath (subPath) {
+  return sysPath.join(
+    __dirname,
+    'test-fixtures',
+    subdir && subdir.toString() || '',
+    subPath
+  )
+}
+
+let subdir = 0,
+    testCount = 1,
+    mochaIt = it,
+    fixturesPath = getFixturePath(''),
+    PERM_ARR = 0x1ed // rwe, r+e, r+e; 755
+
+if (!fs.readFileSync(__filename).toString().match(/\sit\.only\(/)) {
+  it = function() {
+    testCount++
+    mochaIt.apply(this, arguments)
+  }
+  it.skip = function() {
+    testCount--
+    mochaIt.skip.apply(this, arguments)
+  }
+}
+
+before((done) => {
+  rimraf(sysPath.join(__dirname, 'test-fixtures'), function(err) {
+    if (err) throw err
+      fs.mkdir(fixturesPath, PERM_ARR, function(err) {
+        if (err) throw err
+        while (subdir < testCount) {
+          subdir++
+          fixturesPath = getFixturePath('')
+          fs.mkdirSync(fixturesPath, PERM_ARR)
+        }
+        subdir = 0
+        done()
+      })
+  })
+})
+
+beforeEach(function() {
+  subdir++
+  fixturesPath = getFixturePath('')
+})
+
+afterEach(() => {
+  AWS.restore('S3')
+})
+
+const waitFor = (spies) => new Promise((resolve) => {
+  function isSpyReady(spy) {
+    return Array.isArray(spy) ? spy[0].callCount >= spy[1] : spy.callCount
+  }
+  function finish() {
+    clearInterval(intrvl)
+    clearTimeout(to)
+    resolve()
+  }
+  let intrvl = setInterval(function() {
+    if (spies.every(isSpyReady)) finish()
+  }, 5)
+  let to = setTimeout(finish, 3500)
+})
+
+describe('filesystem integration testing', () => {
+
+    it('should add existing files to upload queue when bucket is empty', async () => {
+      AWS.mock('S3', 'headBucket', {})
+      AWS.mock('S3', 'listObjectsV2', {Contents: []})
+
+      // create 3 "blocks"
+      let fixturePath = getFixturePath('')
+      let filenames = new Array(3).fill(1).map((_, i) => sysPath.join(fixturePath, new BN(i).toString(10, BLOCKNUMBER_BYTE_SIZE * 2)))
+      filenames.forEach((file) => {
+        fs.writeFileSync(file, 'b')
+      })
+      const s3Uploader = new S3BlockProducer('test-bucket', fixturePath)
+      await s3Uploader.init()
+      expect(s3Uploader.uploadQueue).to.be.eql(filenames)
+    })
+
+    it('should not duplicate items in the upload queue', async () => {
+      AWS.mock('S3', 'headBucket', {})
+      AWS.mock('S3', 'listObjectsV2', {Contents: [{
+        // put first block into bucket
+        Key: new BN(0).toString(10, BLOCKNUMBER_BYTE_SIZE * 2)
+      }]})
+
+      // create 3 "blocks" locally
+      let fixturePath = getFixturePath('')
+      let filenames = new Array(3).fill(1).map((_, i) => sysPath.join(fixturePath, new BN(i).toString(10, BLOCKNUMBER_BYTE_SIZE * 2)))
+      filenames.forEach((file) => {
+        fs.writeFileSync(file, 'b')
+      })
+      const s3Uploader = new S3BlockProducer('test-bucket', fixturePath)
+      await s3Uploader.init()
+      // first file key is already in S3 so only include last two in queue
+      expect(s3Uploader.uploadQueue).to.be.eql(filenames.slice(1))
+    })
+
+    it('should queue newly added files for upload', async () => {
+      AWS.mock('S3', 'headBucket', {})
+      AWS.mock('S3', 'listObjectsV2', {Contents: []})
+
+      // create a new block file after s3 upload file monitoring has started
+      let fixturePath = getFixturePath('')
+      const newFilePath = sysPath.join(fixturePath, new BN(5).toString(10, BLOCKNUMBER_BYTE_SIZE * 2))
+      // use a file listener to await new file event
+      const fileSpy = sinon.spy()
+      const s3Uploader = new S3BlockProducer('test-bucket', fixturePath, [fileSpy])
+      await s3Uploader.init()
+      // add block number 5 file
+      fs.writeFileSync(newFilePath, 'b')
+      // wait for listener to be called
+      await waitFor([fileSpy])
+      fileSpy.should.have.been.calledOnce
+      fileSpy.should.have.been.calledWith(newFilePath)
+      expect(s3Uploader.uploadQueue).to.be.eql([newFilePath])
+    })
+
+    it('should ensure files are uploaded in ascending order', async () => {
+
+      // create 1 block during initialization
+      let fixturePath = getFixturePath('')
+      const newFilePath = sysPath.join(fixturePath, new BN(5).toString(10, BLOCKNUMBER_BYTE_SIZE * 2))
+
+      AWS.mock('S3', 'headBucket', {})
+      AWS.mock('S3', 'listObjectsV2', (_, callback) => {
+        // create a new block file while polling s3 bucket
+        fs.writeFile(newFilePath, 'b', () => {
+          callback(null, {Contents: []})
+        })
+      })
+
+      // create 3 pre-existing "blocks"
+      let filenames = new Array(3).fill(1).map((_, i) => sysPath.join(fixturePath, new BN(i).toString(10, BLOCKNUMBER_BYTE_SIZE * 2)))
+      filenames.forEach((file) => {
+        fs.writeFileSync(file, 'b')
+      })
+
+      // use a file listener to await new file event
+      const fileSpy = sinon.spy()
+      const s3Uploader = new S3BlockProducer('test-bucket', fixturePath, [fileSpy])
+      await s3Uploader.init()
+      // wait for listener to be called
+      await waitFor([fileSpy])
+      fileSpy.should.have.been.calledOnce
+      fileSpy.should.have.been.calledWith(newFilePath)
+      expect(s3Uploader.uploadQueue).to.be.eql(filenames.concat([newFilePath]))
+    })
+})
+
+
